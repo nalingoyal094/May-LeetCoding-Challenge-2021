a9742360d0aab4d5a63be732f8606ea6834f5b0c
nalingoyal094 <52338664+nalingoyal094@users.noreply.github.com>
2021-05-08 10:11:41 +0530

    Update Delete Operation for Two Strings.cpp

--- Delete Operation for Two Strings.cpp
+++ Delete Operation for Two Strings.cpp
@@ -45,3 +45,72 @@ public:
         
     }
 };
+
+
+
+
+
+
+
+// II approach - without using LCS
+
+class Solution {
+public:
+    
+    int ok(string word1, string word2)
+    {
+        int mx=0;
+        
+        int r=word1.size()+1;
+        int c=word2.size()+1;
+        int a[505][505]={0};
+        
+        for (int i=0;i<=word1.size();i++)
+        {
+            for (int j=0;j<=word2.size();j++)
+            {
+                if (i==0)
+                {
+                    a[i][j]=j;
+                    continue;
+                }
+                
+                if (j==0)
+                {
+                    a[i][j]=i;
+                    continue;
+                }
+                
+                if (word1[i-1]!=word2[j-1])
+                {
+                    a[i][j]=min(a[i-1][j],a[i][j-1])+1;
+                    
+                }
+                else
+                {
+                    a[i][j]=a[i-1][j-1];
+                    
+                  //  mx=max(mx,a[i][j]);
+                }
+                
+                
+            }
+        }
+        return a[word1.size()][word2.size()];
+        
+        
+    }
+    
+    
+    int minDistance(string word1, string word2) {
+        
+        int size=word1.size()+word2.size();
+       //cout<<ok(word1,word2)<<endl;
+        return ok(word1,word2);
+        
+        
+        
+    }
+};
+
+
